31b34dabc5fd9925fb298543daac34cd6f5583ca
C4631 <cjy14524@gmail.com>
2025-07-15 16:36:45 +0800

    Aesthetics
    Changed theme to dark, changed program icon and added keyboard shortcuts

--- src/main/scala-3/ch/makery/address/MainApp.scala
+++ src/main/scala-3/ch/makery/address/MainApp.scala
@@ -10,12 +10,14 @@ import scalafx.scene.Scene
 import scalafx.Includes.*
 import javafx.scene as jfxs
 import scalafx.collections.ObservableBuffer
+import scalafx.scene.image.Image
 import scalafx.stage.{Modality, Stage}
 
 object MainApp extends JFXApp3:
 
   //Window Root Pane
   var roots: Option[scalafx.scene.layout.BorderPane] = None
+  var cssResource = getClass.getResource("view/DarkTheme.css")
   /**
    * The data as an observable list of Persons.
    */
@@ -47,8 +49,11 @@ object MainApp extends JFXApp3:
 
     stage = new PrimaryStage():
       title = "AddressApp"
+      icons += new Image(getClass.getResource("/images/book.png").toExternalForm)
       scene = new Scene():
         root = roots.get
+        stylesheets = Seq(cssResource.toExternalForm)
+        
 
     // call to display PersonOverview when app start
     showPersonOverview()
@@ -74,6 +79,7 @@ object MainApp extends JFXApp3:
       initOwner(stage)
       scene = new Scene:
         root = roots2
+        stylesheets = Seq(cssResource.toExternalForm)
 
     control.dialogStage = dialog
     control.person = person
